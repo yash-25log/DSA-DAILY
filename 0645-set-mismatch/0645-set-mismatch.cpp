e463df8c5976a2798dbcc632375fa9bf9fa247f9
YASH SINGHAL <58944204+yash-25log@users.noreply.github.com>
2022-10-23 23:15:47 +0530

    Time: 98 ms (34.46%), Space: 21.5 MB (66.56%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& arr) {
+        int n = arr.size();
+        
+        sort(arr.begin(),arr.end());
+        int dup = -1, missing = 1;
+        for(int i =1 ; i< n ; i++){
+            if(arr[i] == arr[i-1]){
+                dup = arr[i];
+                
+            }
+            else if(arr[i] > arr[i-1]+1){
+                missing = arr[i-1]+1;
+                
+                
+            }
+        }
+        if(arr[n-1]!= n){
+            missing = n;
+            
+        }
+        return {dup, missing};
+        
+    }
+};
