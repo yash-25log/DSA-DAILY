8625803d9b5118527c34d12baa7a4c03f185e781
YASH SINGHAL <58944204+yash-25log@users.noreply.github.com>
2023-11-19 22:40:47 +0530

    Time: 8 ms (58.69%) | Memory: 18.7 MB (88.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -5,8 +5,8 @@ public:
         int i = 0 ;
         for(int j = 1 ; j < nums.size() ; j++){
             if(nums[j] != nums[i]){
-                i++;
-                nums[i] = nums[j];
+                
+                nums[++i] = nums[j];
             }
         }
         return i + 1;
