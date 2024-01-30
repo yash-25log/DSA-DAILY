795017d837394fb5f4722cf3bc6f76d86f5cb801
YASH SINGHAL <58944204+yash-25log@users.noreply.github.com>
2024-01-30 16:46:11 +0530

    Time: 3 ms (37.86%) | Memory: 7.9 MB (91.83%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int c = 0;
+     for(int i = 1 ; i < s.length(); i++){
+         if(tolower(s[i]) != tolower(s[i-1]) ){
+            c ++;
+         }
+     }
+        return c;
+    
+        
+    }
+};
